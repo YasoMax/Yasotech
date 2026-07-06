6e5f2e845a0663a8f5885c62c2313f5bb6587c90
YasoMax <www.yasooamkah123@gmail.com>
2026-07-06 14:36:50 +0200

    Add user registration functionality in register.php

---
+++ register.php
@@ -0,0 +1,15 @@
+<?php
+include 'db.php';
+
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "success";
+} else {
+    echo "error";
+}
+?>

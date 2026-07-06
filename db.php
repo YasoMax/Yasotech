cb6f3d065d68c5c8106cb24812bc08ae19112011
YasoMax <www.yasooamkah123@gmail.com>
2026-07-06 14:47:55 +0200

    Fix formatting issues in db.php

--- db.php
+++ db.php
@@ -5,7 +5,6 @@ $user = "if0_42300926";
 $pass = "f0DPy9irC1hdki";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
